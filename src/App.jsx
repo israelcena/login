6e5a7fcd97f3eae7788e51d0fddb0657c9724b79
Israel Cena <israelsjm@gmail.com>
2021-08-12 18:49:04 -0300

    rework app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.scss'
+import Login from './components/login/login'
 
 export default function App () {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Login />
     </div>
   )
 }
