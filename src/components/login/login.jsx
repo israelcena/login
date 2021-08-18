515d731900f99e762b52f3a98b4a4b7f4ba9440f
Israel Cena <israelsjm@gmail.com>
2021-08-18 16:44:09 -0300

    rename jsx tags and class names

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
+
 import loginImg from '../../../login.svg'
 
 export default function Login(props) {
   return (
     <div className='container'>
       <div className='header'>Login</div>
-      <div className='Content'>
-        <div className='image'>
-          <img src={loginImg} />
+      <div className='content'>
+        <div className='content-image-box'>
+          <img className='content-image-element' src={loginImg} alt='login-image' />
         </div>
-        <div className='form'>
+        <form className='form'>
           <div className='form-group'>
-            <label htmlFor='username'>Username: </label>
-            <input type='text' name='username' placeholder='username'/>
+            <label htmlFor='username'>Usuário: </label>
+            <input type='text' name='username' placeholder='Digite seu usuário'/>
           </div>
           <div className='form-group'>
-            <label htmlFor='password'>password: </label>
+            <label htmlFor='password'>Password: </label>
             <input type='password' name='password' placeholder='password'/>
           </div>
-        </div>  
+        </form>  
       </div>
-      <div className='footer'>
+      <footer className='footer'>
         <button type='button' className='btn'>Login</button>
-      </div>
+      </footer>
     </div>
   )
 }
