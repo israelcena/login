52e3d629dc0596de9e5effe9cfebcf1f4dca74c0
Israel Cena <israelsjm@gmail.com>
2021-08-23 17:26:41 -0300

    add side for register or login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-import { Login } from './components/login/index'
+import { Login, Register } from "./components/login/Index";
+import Side from "./components/login/Side"
 
 export default function App() {
+  const [isLogginActive, setIsLogginActive] = useState(true);
+
+  const current = isLogginActive ? "Registro" : "Login";
+  const currentActive = isLogginActive ? "login" : "Registro";
+
+  useEffect(() => {
+    Side.classList.add("right");
+  });
+
+  const changeState = () => {
+    if (isLogginActive) {
+      Side.classList.remove("right");
+      Side.classList.add("left");
+    } else {
+      Side.classList.remove("left");
+      Side.classList.add("right");
+    }
+    setIsLogginActive(!isLogginActive);
+  }
+
   return (
-    <div>
-      <Login />
+    <div className="container">
+      <div className="login-container" ref={ref => (container = ref)}>
+        {isLogginActive && (
+          <Login containerRef={ref => (current = ref)} />
+        )}
+        {!isLogginActive && (
+          <Register containerRef={ref => (current = ref)} />
+        )}
+      </div>
+      <Side
+        current={current}
+        currentActive={currentActive}
+        containerRef={ref => (this.Side = ref)}
+        onClick={changeState}
+      />
     </div>
-  )
+  );
 }
