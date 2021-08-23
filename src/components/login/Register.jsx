1a8bd7d4e16c938a30a6cc233d11528c31a55a0a
Israel Cena <israelsjm@gmail.com>
2021-08-23 15:58:50 -0300

    rework register for form component

---
+++ src/components/login/Register.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Form from './Form'
+
+export function Register(props) {
+
+  return (
+    <section className='container' ref={props.containerRef}>
+      <Form header='Registrar' btnTitle='Registrar'>
+        <div className='content-form-group'>
+          <label htmlFor='password'>Confirme a Senha: </label>
+          <input type='password' name='password' placeholder='Confirmação da senha' />
+        </div>
+      </Form>
+    </section>
+  )
+}
